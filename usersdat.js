735f2d5d72e26312e7b1bfcb4f3b1c2e88048dea
rambull <89799958+Rambull178@users.noreply.github.com>
2022-03-19 16:13:34 +0800

    Add files via upload

--- usersdat.js
+++ usersdat.js
@@ -1,7 +1,51 @@
-[
-  {
-    "name": "HikiNeet",
-    "ar":18,
-    "age": 13,
-  }
-]
+ user1 = {
+  "name": "Hiki",
+  "ar": 18,
+  "age": 13,
+}
+user2 = {
+  "name": "His",
+  "ar": 18,
+  "age": 13,
+}
+ user3 = {
+  "name": "Hsi",
+  "ar": 18,
+  "age": 13,
+}
+ user4 = {
+  "name": "Hii",
+  "ar": 18,
+  "age": 13,
+}
+ user5 = {
+  "name": "Rff",
+  "ar": 18,
+  "age": 13,
+}
+ user6 = {
+  "name": "Hijdj",
+  "ar": 18,
+  "age": 13,
+}
+ user7 = {
+  "name": "Himej",
+  "ar": 18,
+  "age": 13,
+}
+ user8 = {
+  "name": "Hejd",
+  "ar": 18,
+  "age": 13,
+}
+ user9 = {
+  "name": "Hsj",
+  "ar": 18,
+  "age": 13,
+}
+ user10 = {
+  "name": "hsh",
+  "ar": 18,
+  "age": 13,
+}
+
